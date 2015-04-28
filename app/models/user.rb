5df2e2d2516a91b81b096863803a77e8f6330918
Tess Griffin <tessjgriffin@gmail.com>
2015-04-28 12:01:09 -0600

    Added validations for display name and added user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  validates :full_name, :role, presence: true
+  validates :full_name, presence: true
   validates :email, uniqueness: true, presence: true, email_format: { message: 'is not looking good'}
+  validates :display_name, length: { in: 2..32 }, allow_blank: true
 
   has_many :item_categories
   has_many :items, through: :item_categories
@@ -11,6 +12,10 @@ class User < ActiveRecord::Base
 
   def admin?
     role == "admin"
+  end
+
+  def display?
+    display_name && display_name != ""
   end
 
 end
