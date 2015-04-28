e53a89e50517b36f12f8c656085bc07969edc4c4
Tess Griffin <tessjgriffin@gmail.com>
2015-04-28 14:06:18 -0600

    User views individual item page

--- spec/features/user_views_all_items_spec.rb
+++ spec/features/user_views_all_items_spec.rb
@@ -5,11 +5,18 @@ feature "user views items index" do
   let(:user) { create(:user) }
 
   scenario "user sees all items" do 
-    create(:item)
+    create(:item, title: "hotdog_1")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit items_path
 
     expect(page).to have_content("hotdog_1")
+  end
+
+  scenario "unauthorized user sees all items" do 
+    create(:item, title: "hotdog_2")
+    visit items_path
+
+    expect(page).to have_content("hotdog_2")
   end
 
 end 
