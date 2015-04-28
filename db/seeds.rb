dc33535bdf4349a01162a9e056e7eb611c26e427
Tess Griffin <tessjgriffin@gmail.com>
2015-04-28 08:38:35 -0600

    Created initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,87 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+class Seed
+
+  attr_reader :rachel, :jeff, :jorge, :josh, :ordered, :paid, :cancelled, :completed
+  def self.start
+    new.generate
+  end
+
+  def generate
+    create_users
+    create_statuses
+    create_items
+    create_orders
+    create_categories
+  end
+
+  def create_users
+    @rachel = User.create!(full_name: "Rachel Warbelow", password_digest: "password", display_name: "", role: 0, email: "demo_rachel@jumpstartlab.com")
+    @jeff = User.create!(full_name: "Jeff Casimir", password_digest: "password", display_name: "j3", role: 0, email: "demo_jeff@jumpstartlab.com")
+    @jorge = User.create!(full_name: "Jorge Tellez", password_digest: "password", display_name: "novohispano", role: 0, email: "demo_jorge@jumpstartlab.com")
+    @josh = User.create!(full_name: "Josh Cheek", password_digest: "password", display_name: "josh", role: 1, email: "demo_josh@jumpstartlab.com")
+
+    puts "#{User.all.map(&:full_name).join(", ")} created."
+  end
+
+  def create_statuses
+    @ordered = Status.create!(name: "ordered")
+    @paid = Status.create!(name: "paid")
+    @cancelled = Status.create!(name: "cancelled")
+    @completed = Status.create!(name: "completed")
+
+    puts "Statuses created"
+  end
+
+  def create_items
+    Item.create!(title: "Sean's Footlong", description: "A hotdog with ketchup", price: 200)
+    Item.create!(title: "Yellow Dog", description: "A hotdog with mustard", price: 250)
+    Item.create!(title: "Sunset Dog", description: "A hotdog with mustard and ketchup", price: 400)
+    Item.create!(title: "Rainbow Dog", description: "A hotdog with relish, ketchup, relish, and mustard", price: 450)
+    Item.create!(title: "Salty Dog", description: "A hotdog on a pretzel bun", price: 600)
+    Item.create!(title: "Wet Dog Smell", description: "A hotdog where the bun has been soaked in vodka", price: 1000)
+    Item.create!(title: "Standard Dog", description: "A hotdog", price: 100)
+    Item.create!(title: "Raining Cats and Dog", description: "A hotdog with a side of rain water", price: 500)
+    Item.create!(title: "Banana Split Dog", description: "A banana on a hot dog bun with whipped cream", price: 700)
+    Item.create!(title: "Sundae", description: "A banana on a hot dog bun with nuts and whipped cream", price: 800)
+    Item.create!(title: "Weiner Dog", description: "A weinerschitzel with a side of sauerkraut", price: 550)
+    Item.create!(title: "Veggie Dog", description: "A lettuce wrap with a veggie hotdog", price: 450)
+    Item.create!(title: "Sinatra Dog", description: "A hotdog that's classy", price: 600)
+    Item.create!(title: "Dancing Dog", description: "A hotdog habeneros and jalapenos", price: 300)
+    Item.create!(title: "Texas Dog", description: "A hotdog with chili", price: 200)
+    Item.create!(title: "Red or Green Dog", description: "A hotdog with your choice of chile", price: 250)
+    Item.create!(title: "Heart Attack Dog", description: "A donut bun with bacon as the meat", price: 1200)
+    Item.create!(title: "Sean's Delicious Sausage", description: "A hotdog with chololate syrup", price: 300)
+    Item.create!(title: "Lasso Dog", description: "A hotdog with fried onions", price: 400)
+    Item.create!(title: "Cowboy Kyle Dog", description: "A hotdog every topping, including chocolate", price: 500)
+  
+    puts "Items created"
+  end
+
+  def create_orders
+    Order.create!(user_id: @rachel.id, status_id: @ordered.id, order_date: DateTime.new(2015, 4, 5), items: [Item.find(16)])
+    Order.create!(user_id: @rachel.id, status_id: @paid.id, order_date: DateTime.new(2015, 4, 6), items: [Item.find(2), Item.find(4)])
+    Order.create!(user_id: @jeff.id, status_id: @ordered.id, order_date: DateTime.new(2015, 4, 7), items: [Item.find(5)])
+    Order.create!(user_id: @jeff.id, status_id: @paid.id, order_date: DateTime.new(2015, 4, 8), items: [Item.find(4)])
+    Order.create!(user_id: @jeff.id, status_id: @completed.id, order_date: DateTime.new(2015, 4, 8), items: [Item.find(14), Item.find(13)])
+    Order.create!(user_id: @jorge.id, status_id: @completed.id, order_date: DateTime.new(2015, 4, 9), items: [Item.find(1)])
+    Order.create!(user_id: @jorge.id, status_id: @ordered.id, order_date: DateTime.new(2015, 4, 10), items: [Item.find(11)])
+    Order.create!(user_id: @jorge.id, status_id: @cancelled.id, order_date: DateTime.new(2015, 5, 5), items: [Item.find(19), Item.find(9)])
+    Order.create!(user_id: @jeff.id, status_id: @cancelled.id, order_date: DateTime.new(2015, 5, 6), items: [Item.find(18)])
+    Order.create!(user_id: @rachel.id, status_id: @paid.id, order_date: DateTime.new(2015, 5, 7), items: [Item.find(19)])
+    puts "Orders created"
+  end
+
+  def create_categories
+    Category.create!(name: "spicy", items: [Item.find(20), Item.find(13), Item.find(16)])
+    Category.create!(name: "dessert", items: [Item.find(10), Item.find(9), Item.find(8), Item.find(17)])
+    Category.create!(name: "kyle's faves", items: [Item.find(5), Item.find(20)])
+    Category.create!(name: "adult", items: [Item.find(6)])
+    Category.create!(name: "puns", items: [Item.find(8), Item.find(5)])
+    puts "Categories Created"
+  end
+  
+end
+
+Seed.start
