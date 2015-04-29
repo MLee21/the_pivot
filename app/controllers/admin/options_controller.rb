793308b9636688c464c138d9fddcade91083e738
end3rs-army <eric.m.dowty@gmail.com>
2015-04-28 20:41:42 -0600

    updates to user profile and orders links

--- app/controllers/admin/options_controller.rb
+++ app/controllers/admin/options_controller.rb
@@ -2,7 +2,7 @@ class Admin::OptionsController < Admin::BaseController
 
   def route
     option = params[:option][:option_type]
-    redirect_to admin_items_path      if option == "items"
+    redirect_to items_path            if option == "items"
     redirect_to admin_orders_path     if option == "orders"
     redirect_to admin_categories_path if option == "categories"
   end
