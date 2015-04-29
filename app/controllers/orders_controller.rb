47d2b1c3a50ccda86faa8d8831af58120659c003
end3rs-army <eric.m.dowty@gmail.com>
2015-04-28 23:56:09 -0600

    updates to user orders, includes all functionality for users to see orders/order details

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,18 @@
 class OrdersController < ApplicationController
 
   def index
-    @orders = current_user.orders
+    @order_counts = current_user.orders_by_status
+    if params.has_key?(:status)
+      @orders = Status.find(params[:status][:status_id]).orders.where(user_id: current_user.id)
+      @list   = Status.find(params[:status][:status_id]).name
+    else
+      @orders = current_user.orders
+      @list   = "all orders"
+    end
+  end
+
+  def show
+    @order = Order.find(params[:id])
   end
 
 end
