0e9ceda99929a22d4ff0425031e527c1d862bc5f
end3rs-army <eric.m.dowty@gmail.com>
2015-04-29 10:41:46 -0600

    fixed the items index and show pages as well

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,9 @@ class Item < ActiveRecord::Base
 
   has_attached_file :image, default_url: "happy_hot_dog.jpg"
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png"]
+
+  def price_formatted
+    "#{sprintf( "$%.02f" , (price.to_f/100))}"
+  end
+
 end
