10c73b90c8d024bccce216b56a9cd3a213528d9b
Tess Griffin <tessjgriffin@gmail.com>
2015-05-02 17:06:44 -0600

    Adds tests for item validity and adds some validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ActiveRecord::Base
   validates :title, :description, :price, presence: true
 
+  validates :title, uniqueness: true
+
+  validates :price, numericality: { greater_than: 0 }
+
   has_many :item_categories
   has_many :categories, through: :item_categories
 
