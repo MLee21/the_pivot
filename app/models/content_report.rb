7f30f90ceb12f0939a007b0740abb2dfb6536ada
end3rs-army <eric.m.dowty@gmail.com>
2015-05-04 11:29:26 -0600

    refactored items, orders, and cart models

--- app/models/content_report.rb
+++ app/models/content_report.rb
@@ -4,4 +4,20 @@ module ContentReport
     "#{sprintf( "$%.02f" , (price.to_f/100))}"
   end
 
+  def item_subtotal(item, quantity)
+    quantity * item.price
+  end
+
+  def item_price(item)
+    to_money_string(item.price)
+  end
+
+  def cart_parse(item, quantity)
+    subtotal = item_subtotal(item, quantity)
+    {subtotal: to_money_string(subtotal),
+     price: item_price(item), 
+     quantity: quantity
+    }
+  end
+
 end
