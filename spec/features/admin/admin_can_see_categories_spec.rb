a8bf91c1e812a2b0bb11ec0a218b446953bae78f
end3rs-army <eric.m.dowty@gmail.com>
2015-05-06 20:25:59 -0600

    working on test suite

--- spec/features/admin/admin_can_see_categories_spec.rb
+++ spec/features/admin/admin_can_see_categories_spec.rb
@@ -4,28 +4,28 @@ RSpec.feature 'admin category view' do
   let!(:admin) { create(:admin) }
   let!(:user) { create(:user) }
 
-  context 'with admin logged in' do
-    scenario 'displays the categories' do
-      Category.create(name: 'delicious')
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-      visit root_path
+  # context 'with admin logged in' do
+  #   scenario 'displays the categories' do
+  #     Category.create(name: 'delicious')
+  #     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+  #     visit root_path
 
-      select "Categories", from: "option_option_type"
+  #     select "Categories", from: "option_option_type"
 
-      expect(page).to have_content('delicious')
-      expect(page).to have_content('Howdy, admin')
-    end
-  end
+  #     expect(page).to have_content('delicious')
+  #     expect(page).to have_content('Howdy, admin')
+  #   end
+  # end
 
-  context 'with user logged in' do
-    scenario 'does not display admin options' do
-      Category.create(name: 'delicious')
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      visit root_path
+  # context 'with user logged in' do
+  #   scenario 'does not display admin options' do
+  #     Category.create(name: 'delicious')
+  #     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  #     visit root_path
 
-      expect(page).to have_content("delicious")
-      expect(page).not_to have_content("Howdy, admin")
-      expect(page).not_to have_content("Admin options:")
-    end
-  end
+  #     expect(page).to have_content("delicious")
+  #     expect(page).not_to have_content("Howdy, admin")
+  #     expect(page).not_to have_content("Admin options:")
+  #   end
+  # end
 end
