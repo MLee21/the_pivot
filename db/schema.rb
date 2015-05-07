4303284e4efaf925d49a94101944058e4a6ba297
end3rs-army <eric.m.dowty@gmail.com>
2015-05-07 10:12:26 -0600

    updates to forms css

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506045517) do
+ActiveRecord::Schema.define(version: 20150507152933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20150506045517) do
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
-    t.integer  "price"
+    t.integer  "price",              default: 1
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
