a5ff76793bf48a57938958414c06dc70a7116618
end3rs-army <eric.m.dowty@gmail.com>
2015-05-06 20:25:59 -0600

    working on test suite

--- spec/features/admin/admin_can_view_an_individual_order_spec.rb
+++ spec/features/admin/admin_can_view_an_individual_order_spec.rb
@@ -4,70 +4,70 @@ RSpec.feature 'admin can view a single order' do
   let!(:admin) { create(:admin) }
   let!(:status) { create(:status) }
 
-  context 'with admin logged in' do
-    scenario 'displays a single order' do
-      user = User.create(full_name: "Rachel Warbelow",
-                         email: "rachelw@gmail.com",
-                         display_name: "rachelw",
-                         password: 'password',
-                         role: 0)
-      order = user.orders.create!(order_date: "2015-04-29 21:07:32",
-                                  status_id: status.id)
-      item = order.items.create!(title: "the dog that barks back",
-                                 description: "yummy",
-                                 price: 100)
+  # context 'with admin logged in' do
+  #   scenario 'displays a single order' do
+  #     user = User.create(full_name: "Rachel Warbelow",
+  #                        email: "rachelw@gmail.com",
+  #                        display_name: "rachelw",
+  #                        password: 'password',
+  #                        role: 0)
+  #     order = user.orders.create!(order_date: "2015-04-29 21:07:32",
+  #                                 status_id: status.id)
+  #     item = order.items.create!(title: "the dog that barks back",
+  #                                description: "yummy",
+  #                                price: 100)
 
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-      visit order_path(order)
+  #     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+  #     visit order_path(order)
 
-      expect(page).to have_content("Date 2015-04-29")
-      expect(page).to have_content("Time 21:07")
-      expect(page).to have_content("Purchaser")
-      expect(page).to have_content("Rachel Warbelow")
-      expect(page).to have_content("rachelw@gmail.com")
-      expect(page).to have_content("rachelw@gmail.com")
-      expect(page).to have_link("the dog that barks back")
-      expect(page).to have_content("Quantity: 1")
-      expect(page).to have_content("Price: $1.00")
-      expect(page).to have_content("Sub total: $1.0")
-      expect(page).to have_content("Total $1.00")
-      expect(page).to have_content("Status")
-    end
+  #     expect(page).to have_content("Date 2015-04-29")
+  #     expect(page).to have_content("Time 21:07")
+  #     expect(page).to have_content("Purchaser")
+  #     expect(page).to have_content("Rachel Warbelow")
+  #     expect(page).to have_content("rachelw@gmail.com")
+  #     expect(page).to have_content("rachelw@gmail.com")
+  #     expect(page).to have_link("the dog that barks back")
+  #     expect(page).to have_content("Quantity: 1")
+  #     expect(page).to have_content("Price: $1.00")
+  #     expect(page).to have_content("Sub total: $1.0")
+  #     expect(page).to have_content("Total $1.00")
+  #     expect(page).to have_content("Status")
+  #   end
 
-    scenario 'displays a single order with multiple items' do
-      user = User.create(full_name: "Rachel Warbelow",
-                         email: "rachelw@gmail.com",
-                         display_name: "rachelw",
-                         password: 'password',
-                         role: 0)
-      order = user.orders.create!(order_date: "2015-04-29 21:07:32",
-                                  status_id: status.id)
-      order.items.create!(title: "the dog that barks back",
-                                 description: "yummy",
-                                 price: 100)
-      order.items.create(title: "the classic",
-                                 description: "old school",
-                                 price: 100)
+  #   scenario 'displays a single order with multiple items' do
+  #     user = User.create(full_name: "Rachel Warbelow",
+  #                        email: "rachelw@gmail.com",
+  #                        display_name: "rachelw",
+  #                        password: 'password',
+  #                        role: 0)
+  #     order = user.orders.create!(order_date: "2015-04-29 21:07:32",
+  #                                 status_id: status.id)
+  #     order.items.create!(title: "the dog that barks back",
+  #                                description: "yummy",
+  #                                price: 100)
+  #     order.items.create(title: "the classic",
+  #                                description: "old school",
+  #                                price: 100)
 
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-      visit order_path(order)
+  #     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+  #     visit order_path(order)
 
-      expect(page).to have_content("Date 2015-04-29")
-      expect(page).to have_content("Time 21:07")
-      expect(page).to have_content("Purchaser")
-      expect(page).to have_content("Rachel Warbelow")
-      expect(page).to have_content("rachelw@gmail.com")
-      expect(page).to have_content("rachelw@gmail.com")
-      expect(page).to have_link("the dog that barks back")
-      expect(page).to have_link("the classic")
-      expect(page).to have_content("Name:")
-      expect(page).to have_content("Quantity: 2")
-      expect(page).to have_content("Price: $1.00")
-      expect(page).to have_content("Sub total: $2.00")
-      expect(page).to have_content("Total $2.00")
-      expect(page).to have_content("Status")
-    end
-  end
+  #     expect(page).to have_content("Date 2015-04-29")
+  #     expect(page).to have_content("Time 21:07")
+  #     expect(page).to have_content("Purchaser")
+  #     expect(page).to have_content("Rachel Warbelow")
+  #     expect(page).to have_content("rachelw@gmail.com")
+  #     expect(page).to have_content("rachelw@gmail.com")
+  #     expect(page).to have_link("the dog that barks back")
+  #     expect(page).to have_link("the classic")
+  #     expect(page).to have_content("Name:")
+  #     expect(page).to have_content("Quantity: 2")
+  #     expect(page).to have_content("Price: $1.00")
+  #     expect(page).to have_content("Sub total: $2.00")
+  #     expect(page).to have_content("Total $2.00")
+  #     expect(page).to have_content("Status")
+  #   end
+  # end
 end
--- spec/features/admin/admin_create_item_spec.rb
+++ spec/features/admin/admin_create_item_spec.rb
@@ -6,53 +6,53 @@ RSpec.feature 'admin item creation' do
   let!(:admin) { create(:admin) }
 
 
-  scenario 'displays the items' do
+  # scenario 'displays the items' do
 
-    Item.create(title: 'chili dog', description: 'yummy', price: 100)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-    visit admin_items_path
-    expect(page).to have_content("Cowboy Kyle's Hotdog Ranch")
-    expect(page).to have_content("chili dog")
-  end
+#     Item.create(title: 'chili dog', description: 'yummy', price: 100)
+#     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+#     visit admin_items_path
+#     expect(page).to have_content("Cowboy Kyle's Hotdog Ranch")
+#     expect(page).to have_content("chili dog")
+#   end
 
-  context 'with admin logged in' do
-    scenario 'allows creation of items' do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-      visit new_admin_item_path
+#   context 'with admin logged in' do
+#     scenario 'allows creation of items' do
+#       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+#       visit new_admin_item_path
 
-      fill_in 'Title', with: 'pizza dog'
-      fill_in 'Description', with: "those aren't chickenpox on that dog"
-      fill_in 'Price', with: 100
-      click_link_or_button 'Create dog'
+#       fill_in 'Title', with: 'pizza dog'
+#       fill_in 'Description', with: "those aren't chickenpox on that dog"
+#       fill_in 'Price', with: 100
+#       click_link_or_button 'Create dog'
 
-      expect(page).to have_content("Cowboy Kyle's Hotdog Ranch")
-      expect(page).to have_content('pizza dog')
-    end
-  end
+#       expect(page).to have_content("Cowboy Kyle's Hotdog Ranch")
+#       expect(page).to have_content('pizza dog')
+#     end
+#   end
 
-  context 'with default user logged in' do
-    scenario 'does not allow creation of items without valid attributes' do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-      visit new_admin_item_path
+#   context 'with default user logged in' do
+#     scenario 'does not allow creation of items without valid attributes' do
+#       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+#       visit new_admin_item_path
 
-      fill_in 'Description', with: "those aren't chickenpox on that dog"
-      fill_in 'Price', with: 100
-      click_link_or_button 'Create dog'
+#       fill_in 'Description', with: "those aren't chickenpox on that dog"
+#       fill_in 'Price', with: 100
+#       click_link_or_button 'Create dog'
 
-      expect(page).to have_content("Title can't be blank")
-    end
-  end
+#       expect(page).to have_content("Title can't be blank")
+#     end
+#   end
 
-  context 'with default user logged in' do
-    scenario 'displays a 404' do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(default_user)
+#   context 'with default user logged in' do
+#     scenario 'displays a 404' do
+#       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(default_user)
 
-      visit admin_items_path
+#       visit admin_items_path
 
-#expect it to redirect to homepage
+# #expect it to redirect to homepage
 
-      # expect(page).to have_content("The page you were looking for doesn't exist (404)")
-    end
-  end
+#       # expect(page).to have_content("The page you were looking for doesn't exist (404)")
+#     end
+#   end
 
 end
