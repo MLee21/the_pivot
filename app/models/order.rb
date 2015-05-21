3e86d4b7c5ca54d8a4a1e6feca09ab125d69d002
Kyle Henson <khenson.kh@gmail.com>
2015-05-20 18:46:46 -0600

    Registered user tests passing

--- app/models/order.rb
+++ app/models/order.rb
@@ -31,7 +31,7 @@ class Order < ActiveRecord::Base
 
   def add_items(cart_contents, order)
     cart_contents.each_pair do |item_id, quantity|
-      quantity.times do 
+      quantity.times do
         order.items << Item.find(item_id)
       end
     end
@@ -60,7 +60,7 @@ class Order < ActiveRecord::Base
   end
 
   def self.generate_order(current_user)
-    status = Status.find_by(name: "ordered")     
+    status = Status.find_by(name: "ordered")
     new(order_date: Time.now, user_id: current_user.id, status_id: status.id )
   end
 
@@ -87,7 +87,7 @@ class Order < ActiveRecord::Base
     hours   = raw_time / 60
     minutes = raw_time % 60
     time_string << "#{hours} hour(s) " if hours > 0
-    time_string << "#{minutes} minutes" 
+    time_string << "#{minutes} minutes"
   end
 
 end
