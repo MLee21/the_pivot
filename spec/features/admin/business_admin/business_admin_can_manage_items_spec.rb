5621d559f169938c3117756748a2e6568e418bc5
MLee21 <minnie.lee89@gmail.com>
2015-05-26 17:15:17 -0600

    writing them tests.

--- spec/features/admin/business_admin/business_admin_can_manage_items_spec.rb
+++ spec/features/admin/business_admin/business_admin_can_manage_items_spec.rb
@@ -7,7 +7,9 @@ feature 'business admin item CRUD functionality' do
     @vendor2 = Vendor.create(name: "Nicholas Cage's Candies")
     @business_admin = BusinessAdministrator.create(full_name:"Gary Johnson", email: "Whatevs@gmail.com", password:"password", password_confirmation: "password", vendor: @vendor)
     @business_admin2 = BusinessAdministrator.create(full_name:"Marilyn Manson", email: "Bloodbath@gmail.com", password:"password", password_confirmation: "password", vendor: @vendor2)
-    @vendor.items.create({title: "Squash", description: "It's good for you", price: 200})
+    category = Category.create(name: "Organic")
+    item = @vendor.items.create({title: "Squash", description: "It's good for you", price: 200})
+    item.categories << category 
     @vendor.items.create({title: "Melon", description: "It's good for you", price: 300})
     @vendor.items.create({title: "Strawberries", description: "It's good for you", price: 400})
     @vendor2.items.create({title: "OMG gummies", description: "Ah", price: 200})
@@ -42,69 +44,53 @@ feature 'business admin item CRUD functionality' do
     expect(page).to_not have_content("OMG gummies")
   end 
 
-  xscenario 'with admin logged in, admin can create items' do
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@business_admin)
-    visit vendors_path
-    click_link "Peter's Produce"
-    expect(current_path).to eq vendor_admin_items_path(vendor)
-
+  scenario 'with admin logged in, admin can create items' do
+    visit root_path
+    click_link "Login/Register"
+    fill_in "session[email]", with: "Whatevs@gmail.com"
+    fill_in "session[password]", with: "password"
+    click_button "Submit"
+    expect(current_path).to eq(admin_dashboard_path)
     expect(page).to have_content("Peter's Produce")
-    click_button "New Item"
+    click_link "Current Items"
+    click_button "Create New Item"
     fill_in "Title", with: "Pumpkin"
     fill_in "Description", with: "Best pumpkins for pumpkin pie makin'"
-    fill_in "Price", with: 500
+    fill_in "Price ($)", with: 5.00
     click_button "Create Item"
 
-    expect(current_path).to eq vendor_admin_items_path(vendor)
+    expect(current_path).to eq(admin_items_path)
     expect(page).to have_content("Squash")
     expect(page).to have_content("Pumpkin")
-    expect(page).to have_link("Pumpkin")
-    expect(page).to have_content("Best pumpkins for pumpkin pie makin'")
     expect(page).to have_content("$5.00")
+    expect(page).to have_link("Edit")
+    expect(page).to have_link("Delete")
   end
 
-  xscenario 'with admin logged in, admin can edit items' do
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@business_admin)
-    visit vendors_path
-    click_link "Peter's Produce"
-    expect(current_path).to eq vendor_admin_items_path(vendor)
-
+  scenario 'with admin logged in, admin can edit items' do
+    visit root_path
+    click_link "Login/Register"
+    fill_in "session[email]", with: "Whatevs@gmail.com"
+    fill_in "session[password]", with: "password"
+    click_button "Submit"
+    expect(current_path).to eq(admin_dashboard_path)
     expect(page).to have_content("Peter's Produce")
+    click_link "Current Items"
     expect(page).to have_content("Squash")
-    expect(page).to have_content("It's good for you")
+    expect(page).to have_content("Melon")
     expect(page).to have_content("$2.00")
-### Needs to be scoped to Edit for Squash
-    click_button "Edit Item"
+    first('.item_box').click_link('Edit')
     fill_in "Title", with: "Tomato"
     fill_in "Description", with: "Don't squeeze 'em'"
-    fill_in "Price", with: 50
-    click_button "Update Item"
+    fill_in "Price", with: 0.50
+    click_button "Update"
 
-    expect(current_path).to eq vendor_admin_items_path(vendor)
+    expect(current_path).to eq(admin_items_path)
     expect(page).not_to have_content("Squash")
     expect(page).to have_content("Tomato")
     expect(page).to have_link("Tomato")
     expect(page).not_to have_content("Best pumpkins for pumpkin pie makin'")
-    expect(page).to have_content("Don't squeeze 'em'")
     expect(page).not_to have_content("$2.00")
-    expect(page).to have_content("$.50")
-  end
-
-  xscenario 'with admin logged in, admin can edit items' do
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@business_admin)
-    visit vendors_path
-    click_link "Peter's Produce"
-    expect(current_path).to eq vendor_admin_items_path(vendor)
-
-    expect(page).to have_content("Peter's Produce")
-    expect(page).to have_content("Squash")
-    expect(page).to have_content("It's good for you")
-    expect(page).to have_content("$2.00")
-### Needs to be scoped to Edit for Squash
-    click_button "Delete Item"
-    expect(page).not_to have_content("Peter's Produce")
-    expect(page).not_to have_content("Squash")
-    expect(page).not_to have_content("It's good for you")
-    expect(page).not_to have_content("$2.00")
+    expect(page).to have_content("$0.50")
   end
 end
