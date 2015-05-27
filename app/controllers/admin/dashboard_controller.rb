004751ac043b1defddf952a8c6849865db3e3c99
Kyle Henson <khenson.kh@gmail.com>
2015-05-26 19:49:28 -0600

    Business Information can be updated by Business Admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,11 +2,11 @@ class Admin::DashboardController < Admin::BaseController
 
   def index
     if current_user.business_administrator?
-      @vendor = current_user.vendor 
+      @vendor = current_user.vendor
       render :business_dashboard
     else
       @vendors = Vendor.all
-      render :platform_dashboardd
+      render :platform_dashboard
     end
   end
 
