ca0abcff6725b860c07275bac16b9956b7dcc9b5
MLee21 <minnie.lee89@gmail.com>
2015-05-26 19:51:48 -0600

    more tests

--- spec/features/admin/business_admin/business_admin_category_management_spec.rb
+++ spec/features/admin/business_admin/business_admin_category_management_spec.rb
@@ -1,2 +1,104 @@
 require "rails_helper"
 
+  feature "a business admin" do 
+
+    before(:each) do
+      @vendor = Vendor.create(name: "Peter's Produce")
+      @vendor2 = Vendor.create(name: "Nicholas Cage's Candies")
+      @business_admin = BusinessAdministrator.create(full_name:"Gary Johnson", email: "Whatevs@gmail.com", password:"password", password_confirmation: "password", vendor: @vendor)
+      @business_admin2 = BusinessAdministrator.create(full_name:"Marilyn Manson", email: "Bloodbath@gmail.com", password:"password", password_confirmation: "password", vendor: @vendor2)
+      category = Category.create(name: "Organic")
+      category2 = Category.create(name: "Juicy")
+      category3 = Category.create(name: "Gluten-Free")
+      item = @vendor.items.create({title: "Squash", description: "It's good for you", price: 200})
+      item2 = @vendor.items.create({title: "Melon", description: "It's good for you", price: 300})
+      item3 = @vendor.items.create({title: "Strawberries", description: "It's good for you", price: 400})
+      item4 = @vendor2.items.create({title: "OMG gummies", description: "Ah", price: 200})
+      item.categories << category 
+      item2.categories << category2
+      item3.categories << category3
+      item4.categories << category3
+    end
+  
+    scenario "logs in and can see all categories associated with their store" do 
+      visit root_path
+      click_link "Login/Register"
+      fill_in "session[email]", with: "Whatevs@gmail.com"
+      fill_in "session[password]", with: "password"
+      click_button "Submit"
+      expect(current_path).to eq(admin_dashboard_path)
+      expect(page).to have_content("Peter's Produce")
+      expect(page).to have_link("View Orders")
+      expect(page).to have_link("Current Items")
+      expect(page).to have_link("Manage Administrators")
+      expect(page).to have_link("Edit Vendor Profile")
+      expect(page).to have_link("Categories")
+    end
+
+    scenario "clicks to see all categories associated with their store" do 
+      visit root_path
+      click_link "Login/Register"
+      fill_in "session[email]", with: "Whatevs@gmail.com"
+      fill_in "session[password]", with: "password"
+      click_button "Submit"
+      click_link "Categories"
+      expect(page).to have_content("Organic")
+      expect(page).to have_content("Juicy")
+      expect(page).to have_content("Gluten-Free")
+    end
+
+    scenario "clicks on a category and sees the items associated with that category for their store only" do 
+      visit root_path
+      click_link "Login/Register"
+      fill_in "session[email]", with: "Whatevs@gmail.com"
+      fill_in "session[password]", with: "password"
+      click_button "Submit"
+      click_link "Categories"
+      click_link "Gluten-Free"
+      expect(page).to have_content("Strawberries")
+      expect(page).to_not have_content("OMG gummies")
+    end
+
+    scenario "edits a category" do 
+      visit root_path
+      click_link "Login/Register"
+      fill_in "session[email]", with: "Whatevs@gmail.com"
+      fill_in "session[password]", with: "password"
+      click_button "Submit"
+      click_link "Categories"
+      click_link "Gluten-Free"
+      click_link "Edit"
+      fill_in "Name", with: "Non-GMO"
+      click_button "Update"
+      expect(page).to_not have_content("Gluten-Free")
+      expect(page).to have_content("Non-GMO")
+    end
+
+    scenario "creates a new category" do 
+      visit root_path
+      click_link "Login/Register"
+      fill_in "session[email]", with: "Whatevs@gmail.com"
+      fill_in "session[password]", with: "password"
+      click_button "Submit"
+      click_link "Categories"
+      click_link "Create New Category"
+      fill_in "Name", with: "Terribly Good for You"
+      click_button "Create Category"
+      expect(current_path).to eq(admin_categories_path)
+      expect(page).to have_content("Terribly Good for You")
+    end
+
+    scenario "deletes a category" do 
+      visit root_path
+      click_link "Login/Register"
+      fill_in "session[email]", with: "Whatevs@gmail.com"
+      fill_in "session[password]", with: "password"
+      click_button "Submit"
+      click_link "Categories"
+      click_link "Organic"
+      click_link "Delete"
+      expect(page).to_not have_content("Organic")
+      expect(page).to have_content("Juicy")
+      expect(page).to have_content("Gluten-Free")
+    end
+  end
